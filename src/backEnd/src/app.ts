39a614fada22df3d4648801922234f463d3efd2b
Antonio Wills <antoniowillissilvasantos@gmail.com>
2025-03-01 16:22:52 -0300

    feat(schema, prisma): add models and add user route

--- src/backEnd/src/app.ts
+++ src/backEnd/src/app.ts
@@ -1,12 +1,31 @@
 import Fastify, { FastifyRequest, FastifyReply } from "fastify";
+import { prisma } from "./config/prisma";
 
 const app = Fastify();
 const PORT = 8080;
 
-app.get("/", async (req: FastifyRequest, reply: FastifyReply) => {
-  reply.send({
-    msg: "Back tá funcionando José",
-  });
+app.get("/api/user", async (req: FastifyRequest, reply: FastifyReply) => {
+  reply.send(await prisma.user.findMany());
+});
+
+app.post("/api/user", async (req: FastifyRequest, reply: FastifyReply) => {
+  const { name, email, passWord, status } = req.body as {
+    name: string;
+    email: string;
+    passWord: string;
+    status: string;
+  };
+
+  try {
+    // const u = await prisma.
+    const user = await prisma.user.create({
+      data: { name, email, passWord, status },
+    });
+    reply.send(user);
+  } catch (error) {
+    console.log(error);
+    reply.status(500).send({ error: "Erro ao criar usuário" });
+  }
 });
 
 const start = async () => {
