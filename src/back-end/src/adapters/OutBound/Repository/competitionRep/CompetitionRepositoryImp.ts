e680377950c2d6728ce8d1d9135c367469c5081d
Antonio Wills <antoniowillissilvasantos@gmail.com>
2025-06-25 09:48:50 -0300

    feat: Adicionar repository para Competition.

---
+++ src/back-end/src/adapters/OutBound/Repository/competitionRep/CompetitionRepositoryImp.ts
@@ -0,0 +1,54 @@
+import Competition from "@domain/entities/competition/Competition";
+import CompetitionRepository from "@domain/entities/competition/CompetitionRepository";
+import prismaCompetitionRepository, {
+  PrismaCompetitionRepository,
+} from "@repository/competitionRep/PrismaCompetitionRepository";
+import Mapper from "@util/Mapper";
+
+export class CompetitionRepositoryImp implements CompetitionRepository {
+  constructor(
+    private readonly prismaCompetitionRepository: PrismaCompetitionRepository
+  ) {}
+
+  async insert(competition: Competition): Promise<Competition | null> {
+    const competitionModel = Mapper.Competition.toPrisma(competition);
+    const compeititonDomain = await this.prismaCompetitionRepository.inserte(
+      competitionModel
+    );
+
+    if (!compeititonDomain) return null;
+
+    return Mapper.Competition.toDomain(compeititonDomain);
+  }
+
+  async findById(id: string): Promise<Competition | null> {
+    const competitionModel = await this.prismaCompetitionRepository.findById(
+      id
+    );
+    if (!competitionModel) return null;
+
+    return Mapper.Competition.toDomain(competitionModel);
+  }
+
+  async findMany(): Promise<Competition[]> {
+    const competitionModels = await this.prismaCompetitionRepository.findMany();
+    return competitionModels.map(Mapper.Competition.toDomain);
+  }
+
+  async update(competition: Competition): Promise<Competition> {
+    const competitionModel = await this.prismaCompetitionRepository.update(
+      Mapper.Competition.toPrisma(competition)
+    );
+    return Mapper.Competition.toDomain(competitionModel);
+  }
+
+  async deleteById(id: string): Promise<Competition | null> {
+    const competitionModel = await this.prismaCompetitionRepository.delete(id);
+    if (!competitionModel) return null;
+    return Mapper.Competition.toDomain(competitionModel);
+  }
+}
+
+const competitionRepository: CompetitionRepository =
+  new CompetitionRepositoryImp(prismaCompetitionRepository);
+export default competitionRepository;
--- src/back-end/src/domain/Entities/competition/CompetitionRepository.ts
+++ src/back-end/src/domain/Entities/competition/CompetitionRepository.ts
@@ -1,9 +1,9 @@
 import Competition from "@domain/entities/competition/Competition";
 
-export interface CompetitionRepository {
+export default interface CompetitionRepository {
   insert(competition: Competition): Promise<Competition | null>;
   findById(id: string): Promise<Competition | null>;
   findMany(): Promise<Competition[]>;
-  update(competition: Competition): Promise<Competition | null>;
+  update(competition: Competition): Promise<Competition>;
   deleteById(id: string): Promise<Competition | null>;
 }
