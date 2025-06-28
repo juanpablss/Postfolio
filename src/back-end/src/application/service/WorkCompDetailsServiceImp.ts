001da5c26c9a28fc54c2c2da4538ab78081bd039
Antonio Wills <antoniowillissilvasantos@gmail.com>
2025-06-28 10:40:48 -0300

    feat(backend): Adicionar service de WorkCompDetails.

---
+++ src/back-end/src/application/service/WorkCompDetailsServiceImp.ts
@@ -0,0 +1,24 @@
+import WorkCompDetails from "@domain/entities/workCompDetails/WorkCompDetails";
+import WorkCompDetailsRepository from "@domain/entities/workCompDetails/WorkCompDetailsRepository";
+import workCompDetailsRepositoryImp from "@repository/workCompDetailsRep/WorkCompDetailsRepository";
+import WorkCompDetailsUseCase from "@useCases/WorkCompDetailsUseCase";
+
+class WorkCompDetailsServiceImp implements WorkCompDetailsUseCase {
+  constructor(
+    private readonly workCompDetailsRepository: WorkCompDetailsRepository
+  ) {}
+
+  async findByCompetitionAndWork(
+    competitionId: string,
+    workId: string
+  ): Promise<WorkCompDetails | null> {
+    return await this.workCompDetailsRepository.findByCompetitionAndWork(
+      competitionId,
+      workId
+    );
+  }
+}
+
+const workCompDetailsServiceImp: WorkCompDetailsUseCase =
+  new WorkCompDetailsServiceImp(workCompDetailsRepositoryImp);
+export default workCompDetailsServiceImp;
