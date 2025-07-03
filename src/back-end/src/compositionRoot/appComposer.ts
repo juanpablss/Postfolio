cbb78ecdc7742b86bae9b37f0d536fb1d50b54bc
Antonio Wills <antoniowillissilvasantos@gmail.com>
2025-07-03 08:59:40 -0300

    refactor(backend) [ appCompose.ts ]: atualizar para adicionar novos modules.

--- src/back-end/src/compositionRoot/appComposer.ts
+++ src/back-end/src/compositionRoot/appComposer.ts
@@ -12,42 +12,50 @@ import configureErrorHandling from "@infrastructure/fastify/ConfigureErrorHandli
 import { IUserRepository } from "@user/domain/entities/IUserRepository";
 import { IPortfolioRepository } from "@portfolio/domain/entities/IPortfolioRepository";
 import { IWorkRepository } from "@work/domain/entities/WorkRepository";
+import { ICompetitionRepository } from "@competition/domain/entities/ICompetitionRepository";
 // services
 import { IUserService } from "@user/service/IUserService";
 import { IPortfolioService } from "@portfolio/service/IPortfolioService";
 import { IWorkService } from "@work/service/IWorkService";
+import { ICompetitionService } from "@competition/service/ICompetitionService";
 
 // Portas de Saída (entre domínios)
 import { IUserPort } from "@user/api/IUserPort";
 import { IPortfolioPort } from "@portfolio/api/IPortfolioPort";
+import { IWorkPort } from "@work/api/IWorkPort";
 
 // --- Importações de Implementações Concretas ---
 // Repositórios
 import { PrismaUserRepository } from "@user/repository/PrismaUserRepository";
 import { PrismaPortfolioRepository } from "@portfolio/repository/PrismaPortfolioRepository";
 import { PrismaWorkRepository } from "@work/repository/PrismaWorkRepository";
+import { PrismaCompetitionRepository } from "@competition/repository/PrismaCompetitionRepository";
 
 // Adaptadores de Saída de Serviço (Portas de Saída)
 import { PortfolioAdapter } from "@portfolio/api/PortfolioAdapter";
 import { UserAdaper } from "@user/api/UserAdapter";
+import { WorkAdapter } from "@work/api/WorkAdapter";
 // ... Outros adaptadores de serviço (ex: para Email, Chat, se User precisar deles)
 
 // Services
 import { UserService } from "@user/service/UserService";
 import { PortfolioService } from "@portfolio/service/PortfolioService";
 import { WorkService } from "@work/service/WorkService";
+import { CompetitionService } from "@competition/service/CompetitionServiceImp";
 // ... Outros Services
 
 // Controladores
 import { UserController } from "@user/inBound/UserController";
 import { PortfolioController } from "@portfolio/inBound/PortfolioController";
 import { WorkController } from "@work/inBound/WorkController";
+import { CompetitionController } from "@competition/inBound/CompetitionController";
 // ... Outros Controladores
 
 // Rotas
 import { UserRoute } from "@user/inBound/UserRoute";
 import { PortfolioRoute } from "@portfolio/inBound/PortfolioRoute";
 import { WorkRoute } from "@work/inBound/WorkRoute";
+import { CompetitionRoute } from "@competition/inBound/CompetitionRoute";
 
 // Handlers
 import { PortfolioUserCreatedHandler } from "@portfolio/handler/PortfolioUserCreatedHandler";
@@ -70,6 +78,10 @@ container
   .bind<IWorkRepository>(TYPES.IWorkRepository)
   .to(PrismaWorkRepository)
   .inSingletonScope();
+container
+  .bind<ICompetitionRepository>(TYPES.ICompetitionRepository)
+  .to(PrismaCompetitionRepository)
+  .inSingletonScope();
 
 // --- 2. BIND dos Adaptadores de Serviço (Portas de Saída entre Domínios) ---
 container
@@ -77,7 +89,7 @@ container
   .to(PortfolioAdapter)
   .inSingletonScope();
 container.bind<IUserPort>(TYPES.IUserPort).to(UserAdaper).inSingletonScope();
-// container.bind<IWor>(TYPES.IUserPort).to(UserAdaper).inSingletonScope();
+container.bind<IWorkPort>(TYPES.IWorkPort).to(WorkAdapter).inSingletonScope();
 
 // --- 3. BIND dos Services (Orquestradores de IService - Implementações de Portas de Aplicação) ---
 container
@@ -91,6 +103,10 @@ container
 container
   .bind<IWorkService>(TYPES.IWorkService)
   .to(WorkService)
+  .inSingletonScope();
+container
+  .bind<ICompetitionService>(TYPES.ICompetitionService)
+  .to(CompetitionService)
   .inSingletonScope();
 
 // --- 4. BIND dos Controladores ---
@@ -107,6 +123,10 @@ container
   .bind<WorkController>(TYPES.WorkController)
   .to(WorkController)
   .inRequestScope();
+container
+  .bind<CompetitionController>(TYPES.CompetitionController)
+  .to(CompetitionController)
+  .inRequestScope();
 
 // Handlers
 container
@@ -118,6 +138,7 @@ interface IApplicationControllers {
   userController: UserController;
   portfolioController: PortfolioController;
   workController: WorkController;
+  competitionController: CompetitionController;
   // ... outros controladores
 }
 
@@ -135,11 +156,15 @@ export class AppComposer {
       TYPES.PortfolioController
     );
     const workController = container.get<WorkController>(TYPES.WorkController);
+    const competitionController = container.get<CompetitionController>(
+      TYPES.CompetitionController
+    );
 
     return {
       userController,
       portfolioController,
       workController,
+      competitionController,
       // ... retorne outras instâncias de controlador
     };
   }
@@ -149,6 +174,7 @@ export class AppComposer {
     UserRoute.register(app, this.controllers.userController);
     PortfolioRoute.register(app, this.controllers.portfolioController);
     WorkRoute.register(app, this.controllers.workController);
+    CompetitionRoute.register(app, this.controllers.competitionController);
   }
 
   public registerHandlers(): void {
