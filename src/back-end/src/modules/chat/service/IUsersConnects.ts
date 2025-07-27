468b941609b7cdc63bf96254a21f622407133a4f
Antonio Wills <antoniowillissilvasantos@gmail.com>
2025-07-27 11:04:24 -0300

    feat(backend) { chat }: Adicionar mapeamento entre users e sockets.

--- src/back-end/src/modules/chat/service/IUsersConnects.ts
+++ src/back-end/src/modules/chat/service/IUsersConnects.ts
@@ -1,8 +1,8 @@
 import { WebSocket } from "ws";
 
 export interface IUsersConnects {
-  connection(userId: string, socket: WebSocket): Promise<WebSocket>;
+  connection(userId: string, socket: WebSocket): Promise<boolean>;
 
   getConnection(userId: string): Promise<WebSocket | null>;
-  deleteConnection(userId: string): Promise<true>;
+  deleteConnection(userId: string): Promise<boolean>;
 }
